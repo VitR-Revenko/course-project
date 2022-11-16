ed4ceaa55e36e22785b2e2e76b8f0815af010c28
VitR-Revenko <re.vitalii002@gmail.com>
2022-11-16 16:44:54 +0200

    modal menu add

--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,20 @@
-(function(){
-  const message = 'html boilerplate';
+(function () {
+  const message = "html boilerplate";
   console.log(`${message}!!!`);
 })();
+
+var modal = document.getElementById("modal");
+
+var btn = document.getElementById("js-modal-open");
+
+var cross = document.getElementById("js-modal-close");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+modal.onclick = function (e) {
+  if ([modal, cross].includes(e.target)) {
+    modal.style.display = "none";
+  }
+};
